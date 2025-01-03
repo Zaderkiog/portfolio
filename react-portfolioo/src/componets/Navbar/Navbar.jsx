468778cde1e94f07aa02be938b57887e87f0ecef
Cristian Oviedo <101075590+Zaderkiog@users.noreply.github.com>
2025-01-03 17:36:16 -0500

    Forth commit

--- react-portfolioo/src/componets/Navbar/Navbar.jsx
+++ react-portfolioo/src/componets/Navbar/Navbar.jsx
@@ -4,10 +4,11 @@ import styles from "./Navbar.module.css"
 import {getImageUrl} from "../../utils";
 
 export const Navbar = () => {
-    return (<nav className={styles.navbar}>
+    return (
+    <nav className={styles.navbar}>
         <a  className={styles.title} href="/">Portfolio</a>
         <div className={styles.menu}>
-            <img className={styles.menuBtn} src=""></img>
+            <img className={styles.menuBtn} src={getImageUrl("nav/menu.png")} alt="menu-button"></img>
             <ul className={styles.menuItems}>
                 <li><a href="#about">About</a></li>
                 <li><a href="#experience">Experience</a></li>
